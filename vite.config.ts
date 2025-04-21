5c1250e3a92e76c58664b72377a4c6e870cceee9
LJS360d <luca.lencioni09@gmail.com>
2025-04-21 07:55:55 +0200

    fix vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: '/foque-slammers/', // for github pages deploy
   plugins: [
     basicSsl()
   ],
